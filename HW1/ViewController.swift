91f5ef998888ca43812b8e063f332f0dd55d2e6d
Karina Chadaeva <chadaeva_k@icloud.com>
2022-12-21 15:21:24 +0300

    Initial Commit

---
+++ HW1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW1
+//
+//  Created by Карина Чадаева on 21.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
